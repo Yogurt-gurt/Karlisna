ad772593c2f2bd86197f2c6cfd36e7bd428e14e7
Yogurt-gurt <142737650+Yogurt-gurt@users.noreply.github.com>
2024-11-29 13:17:15 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,1 +1,32 @@
+<?php
 
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Paksa penggunaan HTTPS jika tidak di lingkungan lokal
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
+    }
+}

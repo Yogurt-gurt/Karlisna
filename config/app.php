5ccd953d92e0cc82bef3d9b36ea4a5b9e43f6209
Yogurt-gurt <142737650+Yogurt-gurt@users.noreply.github.com>
2024-11-29 10:17:59 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://001prj.demura.vahry.my.id'),,
 
     'asset_url' => env('ASSET_URL', '/'),
 
